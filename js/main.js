78ba6a4c6af5bc7af80c5e5fb14c795d43555207
Delgermurun <pdelgermurun@gmail.com>
2015-12-14 13:45:57 +0800

    initialize from saved data

--- js/main.js
+++ js/main.js
@@ -24,6 +24,7 @@
   }
 
   const audioElm = document.getElementById('player');
+  const $urlElm = $('[name=media-url]');
 
   const storage = {
     _prefix: 'ZENCAST_',
@@ -90,26 +91,41 @@
     saveUrl(url);
   }
 
+  function initialize() {
+    const url = storage.get('url');
+    if (url) {
+      $(audioElm).attr('src', url);
+      $urlElm.val(url);
+    }
+
+    const time = storage.get('progress');
+    if (url && time) {
+      audioElm.currentTime = Number(time);
+    }
+  }
+
   $(document).ready(function() {
     $('[data-action=media-form]').on('submit', function submit_media_form_event(event) {
       event.preventDefault();
       insertNewUrl($(this).find('[name=media-url]').val());
     });
 
-    $('[data-action=change-speed]').on('change', function speed_change_even() {
+    $('[data-action=change-speed]').on('change', function speed_change_event() {
       const result = changeSpeed($(this).val());
       if (!result) {
         $(this).val('1');
       }
     });
 
-    $('[data-action=change-time]').on('click', function time_change_even(e) {
+    $('[data-action=change-time]').on('click', function time_change_event(e) {
       e.preventDefault();
       changeTime($(this).val());
     });
 
-    audioElm.ontimeupdate = throttle(function() {
+    audioElm.ontimeupdate = throttle(function time_update_event() {
       saveProgress(audioElm.currentTime);
     }, 5000);
+
+    initialize();
   });
 })(window, document);
